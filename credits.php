9768b9addc6cc3b89a1ce62c3f3e89c332e460cc
Kali <corentin.mercier@epita.fr>
2017-06-11 23:07:21 +0200

    alt pages added

---
+++ credits.php
@@ -0,0 +1,23 @@
+Credits
+
+Le groupe a été crée par Yossra Antari, par la suite le concept et le style de jeu a été développé sur les idées d'Ambroise Michel et James Richet.
+
+Ce jeu a été conçu sur l'idée de James Richet et Ambroise Michel.
+
+Animation: James Richet, Yossra Antari.
+
+Modélisation et programmation ( Monstres, Joueur): James Richet, Yossra Antari.
+
+Modélisation et programmation (Objet): Yossra Antari, Ambroise Michel.
+
+Modélisation (Carte): Ambroise Michel, Yossra Antari.
+
+Modélisation et programmation (Interface des menus): Antonin Bellanger , Ambroise Michel.
+
+Intelligence artificiel: James Richet, Antonin Bellanger.
+
+Sons: Ambroise Michel, Antonin Bellanger.
+
+Site: Ambroise Michel, Yossra Antari.
+
+Réseau: James Richet, Antonin Bellanger.
