6d7c4eb09010e832b9560569afbe65dc0a89f3bd
buymercier <corentin.mercier@epita.fr>
2017-06-14 16:41:44 +0200

    Wiki improved

--- wiki.php
+++ wiki.php
@@ -18,7 +18,29 @@ require('include/header.php');
     </div>
     <div id="menu1" class="tab-pane fade">
       <h3>Nos objets</h3>
-      <p>contenu ici</p>
+
+      <!-- objet 1 -->
+
+      <a class="btn btn-primary" role="button" data-toggle="collapse" href="#1" aria-expanded="false" aria-controls="collapseExample">
+        Objet 1
+      </a>
+      <div class="collapse" id="1">
+        <div class="well">
+          ...1
+        </div>
+      </div>
+
+      <!-- objet 2 -->
+
+      <a class="btn btn-primary" role="button" data-toggle="collapse" href="#2" aria-expanded="false" aria-controls="collapseExample">
+        Objet 2
+      </a>
+      <div class="collapse" id="2">
+        <div class="well">
+          ...2
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
