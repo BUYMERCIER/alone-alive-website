6807bd28c6427052693f401eb3e3af38779b8a44
MrHeliose <poubelle.michel68@gmail.com>
2017-06-14 18:30:58 +0200

    add object

--- wiki.php
+++ wiki.php
@@ -59,9 +59,9 @@ require('include/header.php');
           </div>
           <div id="3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
             <div class="panel-body">
-            Le camp des Antagonistes contient 3 mob "The Antagonist".
-            <img src="../img/Antagonist.png" alt="Antagonist" style="width:width;height:height;">
-            The Antagonist ressemble à un joueur avec une peau noire, un masque et une épée blanche. The Antagonist , est posséder par des esprit malveillant qui détruise petit a petit son âme. Celle si  s’échappe lentement de son corps, créant un halo autour de lui. Cette caractéristique lui donne un aspect unique et le rend reconnaissable rapidement.  
+              Le camp des Antagonistes contient 3 mob "The Antagonist".
+              <img src="../img/Antagonist.png" alt="Antagonist" style="width:width;height:height;">
+              The Antagonist ressemble à un joueur avec une peau noire, un masque et une épée blanche. The Antagonist , est posséder par des esprit malveillant qui détruise petit a petit son âme. Celle si  s’échappe lentement de son corps, créant un halo autour de lui. Cette caractéristique lui donne un aspect unique et le rend reconnaissable rapidement.  
             </div>
           </div>
         </div>
@@ -75,9 +75,9 @@ require('include/header.php');
           </div>
           <div id="4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
             <div class="panel-body">
-            Le Kanima n'a pas de camp. Il se balade dans les forets a la recherche d'adversaire.
+              Le Kanima n'a pas de camp. Il se balade dans les forets a la recherche d'adversaire.
              <img src="../img/Kanima.png" alt="Kanima" style="width:width;height:height;">
-            Le Kanima est inspiré d’une créature surnaturelle qu’on retrouve dans le Bestiaire, il ressemble à un lézard humanoïde, il a des griffes très coupantes sur sa tête et sur la longueur de son dos.  
+             Le Kanima est inspiré d’une créature surnaturelle qu’on retrouve dans le Bestiaire, il ressemble à un lézard humanoïde, il a des griffes très coupantes sur sa tête et sur la longueur de son dos.  
             </div>
           </div>
         </div>
@@ -99,7 +99,9 @@ require('include/header.php');
               </div>
               <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                 <div class="panel-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  <img src="../img/scythe.png" alt="scyhte" style="width:width;height:height;">
+                  "The Scythe of the Archnomage" est un objet qui confère 25 dégats et 10 dégats brut ainsi que 7 points de mobilité.
+                  C'est un objet extrêmement offensif très orienté pour la chasse d'autres joueurs.C'est un objet pour les joueurs ayant un style d'assassin.
                 </div>
               </div>
             </div>
@@ -113,7 +115,9 @@ require('include/header.php');
               </div>
               <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                 <div class="panel-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  <img src="../img/ring.png" alt="ring" style="width:width;height:height;">
+                  Malgré sa petite taille, "the Ring of the reaper" est un objet extrémement puissant si bien utilisé puisque celui-ci tue instantanément tous les ennemis ayant moins de 5% de leur santé.
+                  Si un ennemi (un monstre ou un joueur) est tué par cet effet, "the Ring of the reaper" confère un buff cummulable durant 1 min 30 donnant 5 ou 10 points de vie supplémentaires selon s'il s'agit d'un monstre ou d'un autre joueur. si une nouvelle charge se cumule la durée est remise à 1min30.  
                 </div>
               </div>
             </div>
@@ -127,7 +131,77 @@ require('include/header.php');
               </div>
               <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
                 <div class="panel-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  <img src="../img/heartofmino.png" alt="heart" style="width:width;height:height;">
+                  S'il vous arrive de tuer un des MinoTaurus lors d'un combat corps à corps, vous allez avoir la chance de récupérer son coeur.
+                  "The Heart of Mino" est un objet qui donne 500 points de vie supplémentaires. De plus cet objet soigne le joueur, s'il n'a subit aucun dégât depuis plus de 5 secondes d'un montant équivalent à 5% de ses points de vie 
+                </div>
+              </div>
+            </div>
+             <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    The crown of the Fallen King 
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <img src="../img/crown.png" alt="crown" style="width:width;height:height;">
+                  "the crown of the fallen king" est un objet provenant d'un Roi qui a été décapité par les monstres présent maintenant sur la carte, nous pouvons toujours observer les vestiges de son château à l'horizon.
+                  "the crown of the fallen king" donne 250 points de vie supplémentaires, 8 points de mobilité et 8\% de vol de vie. Gardez la à portée de main, elle peut s'avérer très utile dans les cas de mort imminente. 
+                </div>
+              </div>
+            </div>
+             <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    The FireStone 
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <img src="../img/firestone.png" alt="firestone" style="width:width;height:height;">
+                  "the Firestone" est un objet qui donne 200 points de vie et 5 points de mobilité supplémentaires.
+
+                  Il s'agit d'un objet particulièrement puissant puisqu'il inflige 5 dégats par seconde à toutes les entités à moins de 10 unités de distance.
+
+                  Cet objet est présent de façon aléatoire sur la carte, vous pouvez savoir si un de vos adversaires possède des pouvoirs provenant de la FireStone car celle-ci laisse echapper des particules rouges.
+                </div>
+              </div>
+            </div>
+             <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    The saber of the Antagonist 
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <img src="../img/antagonist-saber.png" alt="antagonist-saber" style="width:width;height:height;">
+                  Tout comme le "mask of the Antagonist",  la seule façon de récupérer cette arme est de tuer le monstre éponyme.
+                  Ce sabre est une arme redoutable et la récupérer pourra vous permettre d'avoir des capacités redoutables face à vos adversaires.
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    The mask of the antagonist  
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <img src="../img/antagonist-mask.png" alt="antagonist-mask" style="width:width;height:height;">
+                  Vous pourrez accéder à cet objet si vous parvenez à tuer le monstre The Antagonist.
+                  "the mask of the antagonist" est un objet qui donne 100 points de vie supplémentaire et 25 dégats.
+                  Il s'agit donc d'un objet assez équilibrés ayant à la fois des caractéristiques offensives et défensives.
                 </div>
               </div>
             </div>
