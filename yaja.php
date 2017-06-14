3130df859978a6d6da28eb577f91a6acbfb652f2
MrHeliose <poubelle.michel68@gmail.com>
2017-06-14 16:06:17 +0200

    Create yaja.php

--- yaja.php
+++ yaja.php
@@ -4,7 +4,20 @@ require('include/header.php');
 
 ?>
 
-<!-- your content here -->
+
+<font size=16><b><u>YAJA! </u></b></font>
+
+YAJA! est un groupe de travail composé de Yossra "Tyrayosaure" Antari, Ambroise "Heliozz" Michel, James "Doomky" Richet et Antonin "Trusto" Bellanger.  Ce groupe fut fondé en novembre 2017 pour le projet que nous devions réalisés dans le cadres de nos études.
+
+ 
+<font size=16><b><u> Origine du nom:</u></b></font>
+
+Nous avons formés un groupe ensemble car nous pensons que nos aptitudes sont complémentaires et nous sommes liés par des valeurs communes comme la rigeur et le désir de faire un bon jeu. Le nom <b>YAJA!</b> vient des initiales de nos prénoms réunis:   <b>Y</b>ossra  <b>A</b>mbroise  <b>J</b>ames <b>A</b>ntonin. De plus, ce nom ressemble beaucoup à "Yata" qui se traduit  par "Génial" en japonais, ce qui sonne plutôt bien. Finalement, <b>YAJA!</b> nous faisais aussi pensé à "SEGA" et son jingle audio mythique. Nous avons donc conserver ce nom: YAJA!.
+
+ 
+<font size=16><b><u> Travaux: </u></b></font>
+
+Pour le moment notre groupe a travaillé sur un seul projet: Alone and Alive . Il s'agit d'un jeu de survie en arène multijoueur toujours en développement.
 
 
 <?php
