fb800b19b14ffc66f935c6b63306dda0fd8a6dfe
MrHeliose <poubelle.michel68@gmail.com>
2017-06-14 16:31:25 +0200

    Create news.php

--- news.php
+++ news.php
@@ -4,7 +4,20 @@ require('include/header.php');
 
 ?>
 
-<!-- your content here -->
+<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Old News
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">Update(26/04)</a></li>
+    <li><a href="#">Update(9/05)</a></li>
+    <li><a href="#">Update(18/05)</a></li>
+    <li><a href="#">Update(14/06)</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
 
 
 <?php
