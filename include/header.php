e42caab14365d8bad48c67a83464c213ef37a954
buymercier <corentin.mercier@epita.fr>
2017-06-14 15:56:53 +0200

    Structure done

--- include/header.php
+++ include/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html class="full" lang="en">
-<!-- Make sure the <html> tag is set to the .full CSS class. Change the background image in the full.css file. -->
+<html class="full" lang="fr">
 
 <head>
 
@@ -60,44 +59,3 @@
         </div>
         <!-- /.container -->
     </nav>
-
-    <div class="main">
-      <div class="col-md-2"></div>
-      <div class="jumbotron col-md-8" style="border-radius: 15px; margin-top: 20px;">
-        <div class="container">
-          <img src="img/logo.png" class="img-responsive"/>
-          <br>
-
-<p style="white-space: pre-wrap;">
-Mise à jour disponible ! (26/04/2017)
-
-- Carte plus grande et perfectionnée.
-
-- Les objets sont effectifs
-
-- Première version du système de buff ( bonus)
-
-- Nouveau monstre ajouté + nouvelles textures sur tout les monstres déjà présent
-
-- Animation des monstres ajoutés.
-
-- IA monstres ajoutés .
-
-- Site web disponible.
-</p>
-
-        </div>
-      </div>
-      <div class="col-md-2"></div>
-    </div>
-
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require('include/header.php');
+
+?>
+
+    <div class="main">
+      <div class="col-md-2"></div>
+      <div class="jumbotron col-md-8" style="border-radius: 15px; margin-top: 20px;">
+        <div class="container">
+          <img src="img/logo.png" class="img-responsive"/>
+          <br>
+
+<p style="white-space: pre-wrap;">
+Mise à jour disponible ! (26/04/2017)
+
+- Carte plus grande et perfectionnée.
+
+- Les objets sont effectifs
+
+- Première version du système de buff (bonus)
+
+- Nouveau monstre ajouté + nouvelles textures sur tout les monstres déjà présent
+
+- Animation des monstres ajoutés.
+
+- IA monstres ajoutés .
+
+- Site web disponible.
+</p>
+
+        </div>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+    
+
+<?php
+
+require('include/footer.php');
