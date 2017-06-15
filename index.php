f6c93db1280c53f6bc85b8b7615c7d1bb89280ee
MrHeliose <poubelle.michel68@gmail.com>
2017-06-15 23:54:27 +0200

    debug

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ require('include/header.php');
 ?>
 <font size=16><b><u>Bienvenue sur notre site! </u></b></font>
 
-Il s'agit du site officiel du jeu Alone&Alive dévellopé par notre equipe: YAJA! 
-Alone&Alive est un jeu de survie en arene multijoueur. Votre but? survivre...Le moyen? se battre en PvP et PvE!
+Il s'agit du site officiel du jeu Alone &amp; Alive dévellopé par notre equipe: YAJA! 
+Alone &amp; Alive est un jeu de survie en arene multijoueur. Votre but? survivre...Le moyen? se battre en PvP et PvE!
 
-Vous avez tous les droit: Piller, assassiner, se cacher, fuire... mais n'oubliez pas le but:<b> rester en vie!<\b>
+Vous avez tous les droit: Piller, assassiner, se cacher, fuire... mais n'oubliez pas le but: <b> rester en vie! <\b>
 <div class="panel panel-default">
   <div class="panel-heading">
     <h3 class="panel-title">News!</h3>
